cefbe748c369632ba7fb582038f40ed6c3c7866e
alicexpirvulescu <maria.alice.pirvulescu@gmail.com>
2026-02-28 12:19:12 +0200

    Seminar2

---
+++ Seminar2/src/ro/ase/seminar2/clase/readers/StudentReader.java
@@ -0,0 +1,37 @@
+package ro.ase.seminar2.clase.readers;
+
+import ro.ase.seminar2.clase.Aplicant;
+import ro.ase.seminar2.clase.Student;
+import ro.ase.seminar2.clase.readers.AplicantiReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StudentReader extends AplicantiReader {
+
+    @Override
+    public List<Aplicant> readAplicanti(String file) throws FileNotFoundException {
+
+        Scanner input = new Scanner(new File(file));
+        input.useDelimiter(",|\n");
+        List<Aplicant> studenti = new ArrayList<>();
+
+        while (input.hasNext()) {
+            Student s = new Student();
+
+            super.citesteAplicant(input,s);
+            int an_studii = input.nextInt();
+            s.setAn_studii(an_studii);
+
+            String facultate = (input.next()).toString();
+            s.setFacultate(facultate);
+
+            studenti.add(s);
+        }
+        input.close();
+        return studenti;
+    }
+}
---
+++ Seminar2/src/ro/ase/seminar2/main/Program.java
@@ -0,0 +1,30 @@
+package ro.ase.seminar2.main;
+
+import ro.ase.seminar2.clase.Angajat;
+import ro.ase.seminar2.clase.Aplicant;
+import ro.ase.seminar2.clase.Elev;
+import ro.ase.seminar2.clase.Student;
+import ro.ase.seminar2.clase.readers.AngajatiReader;
+import ro.ase.seminar2.clase.readers.AplicantiReader;
+import ro.ase.seminar2.clase.readers.PupilReader;
+import ro.ase.seminar2.clase.readers.StudentReader;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Program {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        List<Aplicant> listaAngajati;
+        AplicantiReader ra = new AngajatiReader();
+
+        listaAngajati = ra.readAplicanti("angajati.txt");
+
+        for(Aplicant aplicant:listaAngajati)
+            System.out.println(aplicant.toString());
+    }
+
+}
